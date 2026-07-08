a01d5c2974c36c93d86fb254327ec7bd1e0ba03e
Hitesh Tara <hht179@gmail.com>
2026-07-08 14:13:11 -0400

    Add networking module

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -75,3 +75,39 @@ output "dynamodb_table" {
 output "lambda_name" {
   value = module.lambda.function_name
 }
+
+module "networking" {
+  source = "../../../terraform/modules/networking"
+
+  project_name = var.project_name
+  environment  = local.environment
+
+  vpc_cidr = "10.0.0.0/16"
+
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+}
+
+output "vpc_id" {
+  value = module.networking.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.networking.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.networking.private_subnet_ids
+}
